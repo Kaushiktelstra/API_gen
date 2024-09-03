aef199909f99f852dad195a14e873e992ee667fd
Kaushik <MKaushik.Kaushik@team.telstra.com>
2024-09-03 14:09:38 +0530

    Commit

--- apigateway/index.js
+++ apigateway/index.js
@@ -5,19 +5,55 @@ app.use(express.json());
 app.use(cors("*"));
 const axios= require("axios")
 
+const jwt= require("jsonwebtoken")
+const SECK= require("../authdetails/auth");
+function authenticate(key){
+    try{
+        const validated= jwt.verify(key, SECK);
+        return validated;
+    }catch(error){
+        console.log(error);
+    }
+}
+
 app.use( async (req,res, next)=>{
-    const servicename= req.path.split("/")[1];
-    console.log(servicename.data);
+    const authkey= req.header("x-token");
+    if(authenticate(authkey)){
+        console.log("Authenticated!")
+        const resdata= await axios.get("http://localhost:3010/services") //registry URL. 
+        console.log(resdata.data);
+        const action_name= req.path.split("/")[1];
+        console.log(action_name);
+    
+        const servicedata= resdata.data;
+        const serviceinfo= servicedata.find((e)=>e.servicename== action_name)
 
-    const serviceres= await axios.get(`http://localhost:3010/getservice/${servicename}`);
-    console.log(serviceres.data);
+        if(serviceinfo){
+        console.log(serviceinfo);
+        const serviceurl = serviceinfo.url+"/"+ req.path.split("/")[2];
+        const alldata= await axios.get(serviceurl);
+        res.send(alldata.data);
+        }
+        else{
+            res.send("Service info not found")
+        }
+    }
+    else{
+        res.send("Authentication failed")
+}
 
-    const allcoll= await axios.get(serviceres.data.url);
-    res.send(allcoll.data);
+    // const servicename= req.path.split("/")[1];
+    // console.log(servicename.data);
+
+    // const serviceres= await axios.get(`http://localhost:3010/getservice/${servicename}`);
+    // console.log(serviceres.data);
+
+    // const allcoll= await axios.get(serviceres.data.url);
+    // res.send(allcoll.data);
 })
 
 
 
-app.listen(3000, ()=>{
-    console.log("API gateway: 3000")
+app.listen(3001, ()=>{
+    console.log("API gateway running in port 3001")
 })
